652fa397b9bbaa14ee8aef0cd3b4ae80466202e5
WeihaoMo1 <weihaomo520@gmail.com>
2024-09-25 17:21:36 +0200

    fix: minor change

--- code_and_samples/main.c
+++ code_and_samples/main.c
@@ -36,53 +36,6 @@ unsigned char eroded_image[BMP_WIDTH][BMP_HEIGTH];
 
 
 
-void analysis_loop(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH])
-{
-  unsigned char(*current_image)[BMP_HEIGTH] = input_image;
-  unsigned char(*next_image)[BMP_HEIGTH] = output_image;
-
-  int count = 0;
-  int eroded = 1;
-
-  while (eroded)
-  {
-    if (count % 2 == 0 || count == 0)
-    {
-      eroded = erode_image(current_image, next_image);
-      if (!eroded)
-      {
-        break;
-      }
-      count_cells(next_image, current_image);
-    }
-    else
-    {
-      eroded = erode_image(next_image, current_image);
-      if (!eroded)
-      {
-        break;
-      }
-      count_cells(current_image, next_image);
-    }
-    count++;
-  }
-
-  printf("Times eroded: %d\n", count);
-
-  // Just for debugging
-  if (count % 2 == 0 || count == 0)
-  {
-    for (int i = 0; i < BMP_WIDTH; i++)
-    {
-      for (int j = 0; j < BMP_HEIGTH; j++)
-      {
-        output_image[i][j] = input_image[i][j];
-      }
-    }
-  }
-}
-
-
 
 void printMemoryUsage()
 {
